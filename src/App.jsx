145e2b1d45f1a6bbda80ab74db86ac499b3f2023
Your Name <chanrhamni>
2023-02-18 10:57:41 +0530

    update all file

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useReducer, useEffect } from "react";
+
+const App = () => {
+
+
+  return (
+    <div className="App">
+      <h1>Mobile Store</h1>
+    </div>
+  );
+};
+
+export default App;
